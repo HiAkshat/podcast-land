2eab0c618fe59cccc4f621242f15131a2a2218d5
Akshat Gupta <akshatgupta300@gmail.com>
2023-06-29 14:59:12 +0530

    shapes added to episodes

--- src/components/episodes/episodes.jsx
+++ src/components/episodes/episodes.jsx
@@ -34,7 +34,8 @@ const episodes = [
 
 export default function Episodes() {
   return (
-    <div className="flex flex-col bg-[#F1F5F9] rounded-[24px] py-[80px] pl-[80px] mb-[80px]">
+    <div className="relative flex flex-col bg-[#F1F5F9] rounded-[24px] py-[80px] pl-[80px] mb-[80px]">
+      <img className="absolute left-[64px] top-[-34px] w-[66px]" src="./assets/Shapes.svg" alt="" />
       <div className="flex overflow-hidden">
         {episodes.map((episode) => (
           <div className="even:rotate-1 odd:rotate-[-1deg] mr-[28px]">
